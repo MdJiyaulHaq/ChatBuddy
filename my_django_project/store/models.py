20a1595c5c5d1ac1c8000944b5037fec25318263
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2023-03-05 15:53:50 +0545

    model classes created

--- my_django_project/store/models.py
+++ my_django_project/store/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateField(auto_now=True)
+
+
+class Customer (models.Model):
+    membership_bronze = 'B'
+    membership_silver = 'S'
+    membership_gold = 'G'
+    membership_choices = [
+        (membership_bronze, 'Bronze'),
+        (membership_silver, 'Silver')
+        (membership_gold, 'Gold')
+    ]
+    fname = models.CharField(max_length=255)
+    lname = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+
+
+membership = models.CharField(
+    max_length=1, choices=membership_choices, default=membership_bronze)
+
+
+class Order(models.Model):
+    placed_at = models.DateTimeField(auto_now_add=True)
+
+    status_pending = 'P'
+    status_complete = 'C'
+    status_failed = 'F'
+
+    pament_choices = [
+        (status_pending, 'Pending'),
+        (status_complete, 'Complete'),
+        (status_failed, 'Failed')
+    ]
+    payment_status = models.CharField(
+        max_length=1, choices=payment_choices, default=status_pending)

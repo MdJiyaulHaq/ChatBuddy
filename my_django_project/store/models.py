bd9a58cc2d6eb828238bfb386589b8116eec054e
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2023-03-06 23:40:43 +0545

    resovling circular relationship

--- my_django_project/store/models.py
+++ my_django_project/store/models.py
@@ -3,12 +3,27 @@ from django.db import models
 # Create your models here.
 
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
+    """ it is not a good habit to write another class in parnthesis
+"""
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateField(auto_now=True)
+    promotions = models.ManyToManyField(Promotion)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
 
 
 class Customer (models.Model):
@@ -52,7 +67,7 @@ class Address(models.Model):
     city = models.CharField(max_length=255)
     customer = models.OneToOneField(
         Customer, on_delete=models.CASCADE, primary_key=True)
-    
+
 
 class Phone(models.Model):
     phone = models.CharField(max_length=255)

fc4b0ff510d970cfdc1b67ad96b7fc8639477613
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2023-03-07 00:14:04 +0545

    Generic relationship

---
+++ my_django_project/likes/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import *
+from django.contrib.contenttypes import *
+from django.contrib.contenttypes.fields import GenericForeignKey
+# Create your models here.
+
+
+class LikedItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField(max_length=255)
+    content_object = GenericForeignKey()
+
+
+class User(models.Model):
+    item = models.TextField(max_length=255)

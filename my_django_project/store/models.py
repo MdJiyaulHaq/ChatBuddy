2c2e421ca60e1d6fa6bbfd66b75423aba6e7bcdb
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2023-03-08 07:52:08 +0545

    migrations created

--- my_django_project/store/models.py
+++ my_django_project/store/models.py
@@ -38,7 +38,6 @@ class Customer (models.Model):
     fname = models.CharField(max_length=255)
     lname = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)

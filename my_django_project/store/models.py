a557339955952dee79fd270887b3969c2a1be805
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2023-03-15 21:29:44 +0545

    g

--- my_django_project/store/models.py
+++ my_django_project/store/models.py
@@ -76,6 +76,3 @@ class Address(models.Model):
 class Phone(models.Model):
     phone = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-
-class Test:
-    name = 'Md'

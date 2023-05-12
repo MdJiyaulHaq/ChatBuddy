d6fadfc776a5cfbed76c1c88850156bb165f8eaf
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-05-12 20:42:57 +0545

    new start

--- project_mart/BaseApp/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
-
-def say_hello(request):
-    return render(request, 'hello.html', {'name':'Md'})

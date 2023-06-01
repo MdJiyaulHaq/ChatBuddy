a4c0282f6dd6b89189a599745757a97b608b2d0f
Md Jiyaul Haq <69068755+MdJiyaulHaq@users.noreply.github.com>
2023-06-01 12:00:02 +0545

    models created

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -7,10 +8,40 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
- 
-class Custome(models.Model):
+
+
+class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField()
-    birth_date = models.DateField(null=True) 
+    birth_date = models.DateField(null=True)
+
+    MEMBERSHIP_BRONZE = "B"
+    MEMBERSHIP_SILVER = "S"
+    MEMBERSHIP_GOLD = "G"
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, "BRONZE"),
+        (MEMBERSHIP_SILVER, "SILVER"),
+        (MEMBERSHIP_GOLD, "GOLD"),
+    ]
+
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
+    )
+
+
+class Order:
+    placed_at = models.DateField(auto_now_add=True)
+    PAYMENT_STAUS_PENDING = "P"
+    PAYMENT_STAUS_COMPLETE = "C"
+    PAYMENT_STAUS_FAILED = "F"
+    PAYMENT_STAUS_CHOICES = [
+        (PAYMENT_STAUS_PENDING, "PENDING"),
+        (PAYMENT_STAUS_COMPLETE, "COMPLETE"),
+        (PAYMENT_STAUS_FAILED, "FAILED"),
+    ]
+    payment_status = models.CharField(
+        max_length=255, choices=PAYMENT_STAUS_CHOICES, default=PAYMENT_STAUS_PENDING
+    )

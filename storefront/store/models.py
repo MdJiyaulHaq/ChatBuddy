f1c3389051f8c8d465aaecd9db0322feda8963b2
Md Jiyaul Haq <69068755+MdJiyaulHaq@users.noreply.github.com>
2023-06-06 22:28:55 +0545

    customized db schema

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -20,7 +20,7 @@ class Promotion(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.SlugField(default='-')
+    slug = models.SlugField(default="-")
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
@@ -50,6 +50,10 @@ class Customer(models.Model):
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
     )
+
+    class Meta:
+        db_table = "store_customers"
+        indexes = [models.Index(fields=["last_name", "first_name"])]
 
 
 class Order(models.Model):

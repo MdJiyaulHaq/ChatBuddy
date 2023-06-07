e73426e9dedb6c6434a67561d354257cede958e3
Md Jiyaul Haq <69068755+MdJiyaulHaq@users.noreply.github.com>
2023-06-07 23:53:20 +0545

    meta data

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -20,7 +20,7 @@ class Promotion(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.SlugField(default='-')
+    slug = models.SlugField(default="-")
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
@@ -50,6 +50,9 @@ class Customer(models.Model):
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
     )
+
+    class Meta:
+        indexes = [models.Index(fields=["first_name", "last_name"])]
 
 
 class Order(models.Model):

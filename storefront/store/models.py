e111268025b2d4899f5555c6beb22c8c9e67ead7
Md Jiyaul Haq <69068755+MdJiyaulHaq@users.noreply.github.com>
2023-06-12 22:59:40 +0545

    phone field of customer class deleted.

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -34,7 +34,6 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=10)
     birth_date = models.DateField(null=True)
 
     MEMBERSHIP_BRONZE = "B"

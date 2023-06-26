e4a53c65d5d49840d342b5f6f7e42a72081163f3
Md Jiyaul Haq <69068755+MdJiyaulHaq@users.noreply.github.com>
2023-06-26 19:51:51 +0545

    list display

--- tennisclub/members/admin.py
+++ tennisclub/members/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Member
 
 
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("firstname", "lastname")
+
+
 # Register your models here.
 admin.site.register(Member)

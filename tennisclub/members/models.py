91f5ecd5ff3707fa92177c38ef0e16d3c73330b2
Md Jiyaul Haq <69068755+MdJiyaulHaq@users.noreply.github.com>
2023-06-26 19:33:22 +0545

    model added

--- tennisclub/members/models.py
+++ tennisclub/members/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
-class Members(models.Model):
+class Member(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=255)
-    birth_date = models.DateField(null=True)

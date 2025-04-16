5ad2dccc89539a7d722a8320e6039f8c2a027a0c
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-04-16 20:45:50 +0545

    Increase max_length of Topic name field from 100 to 200 characters

--- base/models.py
+++ base/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class Topic(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 

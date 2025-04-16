6f2c6543211aa9ba9f438826180cdc44ae92cd2c
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-04-16 20:42:15 +0545

    Add Room, Topic, and Message models with admin registration and migrations

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Room
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
 
 # Register your models here.
+@admin.register(Room)
+class RoomAdmin(ModelAdmin):
+    list_display = ("id", "name", "description", "updated", "created")
+    list_display_links = ("id", "name")
+    search_fields = ("name", "description")
+    list_filter = ("updated", "created")
+    list_per_page = 10
+    ordering = ("-updated",)
+    readonly_fields = ("created", "updated")
--- base/models.py
+++ base/models.py
@@ -1,3 +1,38 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True, blank=True)
+    host = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:20]

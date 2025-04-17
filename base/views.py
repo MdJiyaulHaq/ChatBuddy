039288877f0070bd828ef0f86633b3a6584ad1b1
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-04-17 13:22:45 +0545

    Add RoomForm and createRoom view for room creation; update home and room templates

--- base/views.py
+++ base/views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, redirect, render
+from base.forms import RoomForm
 from base.models import Room
 
 
 def home(request):
-    rooms = Room.objects.all()
+    rooms = get_list_or_404(Room)
     context = {"rooms": rooms}
     return render(request, "base/home.html", context)
 
 
 def room(request, pk):
-    room = Room.objects.get(id=pk)
+    room = get_object_or_404(Room, pk=pk)
     context = {"room": room}
     return render(request, "base/room.html", context)
+
+
+def createRoom(request):
+    form = RoomForm()
+
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)

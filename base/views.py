b60cfaf2412cb40a87666ee1e6de55b2fa5aa6c9
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-04-17 13:26:35 +0545

    Implement updateRoom view and corresponding URL; add Edit Room link in home template

--- base/views.py
+++ base/views.py
@@ -26,3 +26,17 @@ def createRoom(request):
 
     context = {"form": form}
     return render(request, "base/room_form.html", context)
+
+
+def updateRoom(request, pk):
+    room = get_object_or_404(Room, pk=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)

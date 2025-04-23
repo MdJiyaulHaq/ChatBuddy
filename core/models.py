724fda868a6a4d344282a4bac332ec3c819ec965
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-04-23 17:24:18 +0545

    Switches user login to use email as username
    Configures authentication to require email as the unique identifier,
    streamlining login and registration by removing additional required fields.

--- core/models.py
+++ core/models.py
@@ -5,3 +5,5 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
